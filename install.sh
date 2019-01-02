05f693a271355ff1ce1f2f18361f0230293ea3a2
Santiago Verdu <santiago.verdu.01@gmail.com>
2019-01-02 13:47:11 -0600

    commitall commit

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ fi
 ## Just appends the args to the end of the command
 ## supply domain list to be added to this command:
 ## -d example.com [[-d arg], ...]
-docker run -p80:80 --rm --name $container_name verdude/certbot certbot certonly --agree-tos -m $EMAIL --keep --standalone --no-eff-email -n $@
+docker run -p80:80 --name $container_name verdude/certbot certbot certonly --agree-tos -m $EMAIL --keep --standalone --no-eff-email -n $@
 
 echo "Copying certificates"
 docker cp $container_name:/etc/letsencrypt/ /etc/
