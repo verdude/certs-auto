fdd8a1925d5b800411ba2652b204190e695f1e0c
Santiago Verdu <santiago.verdu.01@gmail.com>
2019-01-02 13:19:29 -0600

    commitall commit

--- install.sh
+++ install.sh
@@ -7,14 +7,16 @@ if [ -z "$EMAIL" ]; then
     exit 1
 fi
 
+container_name=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`
+
 ## Just appends the args to the end of the command
 ## supply domain list to be added to this command:
 ## -d example.com [[-d arg], ...]
-docker run -p80:80 --rm --name certbot_container verdude/certbot certbot certonly --agree-tos -m $EMAIL --keep --standalone --no-eff-email -n $@
+docker run -p80:80 --rm --name $container_name verdude/certbot certbot certonly --agree-tos -m $EMAIL --keep --standalone --no-eff-email -n $@
 exitcode=$?
 
 if [ $exitcode -eq 0 ]; then
-    docker cp certbot_container:/etc/letsencrypt/ /etc/
+    docker cp $container_name:/etc/letsencrypt/ /etc/
 fi
 
 exit $exitcode
